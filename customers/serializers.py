fb29aa14a12727229bc84854e9954158ad2518f2
gabrieljaooo5 <joaogabriel1598@gmail.com>
2023-01-10 09:46:41 -0300

    Check if data is valid

--- customers/serializers.py
+++ customers/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from customers.models import Customer
+from customers.validators import *
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
+
+    def validate(self, data):
+        if not valid_cpf(data['cpf']):
+            raise serializers.ValidationError({'cpf':"The CPF must have 11 digits."})
+        if not valid_name(data['name']):
+            raise serializers.ValidationError({'name':"Do not include numbers in this field."})
+        return data
---
+++ customers/validators.py
@@ -0,0 +1,5 @@
+def valid_cpf(cpf_number):
+    return len(cpf_number) == 11
+
+def valid_name(name):
+    return name.isalpha()
